2c886565121e0293cf95f315f32b5428cf1cb107
esmairemtek <esmairemtek12@gmail.com>
2024-06-15 01:34:57 +0300

    added inheritance to users

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -2,18 +2,13 @@ package model;
 
 import java.sql.Date;
 
-public class Customer {
-private int id;
-private String name;
-private String email;
-private String phone;
-private Date birthDate;
+public class Customer extends User{
 
-    public Customer(int id, String name, String email, String phone, Date birthDate) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.phone = phone;
-        this.birthDate = birthDate;
+    public Customer(int id, String name, String email, String password) {
+        super(id,name,email,password);
+    }
+
+    public Customer() {
+
     }
 }
---
+++ src/model/Manager.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Manager extends User {
+
+    public Manager(int id, String name, String email, String password) {
+        super(id, name, email, password);
+    }
+
+    public Manager() {
+        super();
+    }
+}

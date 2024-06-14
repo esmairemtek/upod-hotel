17878fe331d3bf362bb8208e4aa8b042f1cbf0c9
esmairemtek <esmairemtek12@gmail.com>
2024-06-15 01:35:13 +0300

    exceptions

---
+++ src/dao/ex/DaoException.java
@@ -0,0 +1,16 @@
+package dao.ex;
+
+public class DaoException extends Exception{
+
+    public DaoException(String message) {
+        super(message);
+    }
+
+    public DaoException(Throwable cause) {
+        super(cause);
+    }
+
+    public DaoException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/dao/ex/NoSuchUserException.java
@@ -0,0 +1,9 @@
+package dao.ex;
+
+public class NoSuchUserException extends Exception {
+    private static final String MESSAGE = "No such user found with id ";
+
+    public NoSuchUserException(int id) {
+        super(MESSAGE + id);
+    }
+}

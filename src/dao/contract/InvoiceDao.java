4ddec6fcc50a7b437581dbb92ebce3b41e5bc66a
esmairemtek <esmairemtek12@gmail.com>
2024-06-17 23:10:15 +0300

    invoice dao initialize

--- src/dao/contract/InvoiceDao.java
+++ src/dao/contract/InvoiceDao.java
@@ -1,4 +1,14 @@
 package dao.contract;
 
-public interface InvoiceDao {
+import model.Invoice;
+
+import java.util.List;
+
+public interface InvoiceDao extends AutoCloseable {
+    void save(Invoice invoice);
+    void update(Invoice invoice);
+    void deleteById(Integer id);
+    void findByInvoiceId(Integer id);
+    void findByCustomerId(Integer id);
+    List<Invoice> findAll();
 }
--- src/dao/impl/InvoiceDaoJdbc.java
+++ src/dao/impl/InvoiceDaoJdbc.java
@@ -1,4 +1,51 @@
 package dao.impl;
 
-public class InvoiceDaoJdbc {
+import dao.contract.InvoiceDao;
+import model.Invoice;
+import util.DBUtils;
+
+import java.sql.Connection;
+import java.util.List;
+
+public class InvoiceDaoJdbc implements InvoiceDao {
+
+    private Connection conn;
+    private Connection getConnection() {
+        conn = DBUtils.getConnection();
+        return conn;
+    }
+
+    private static final String SAVE_QUERY = "INSERT INTO INVOICE VALUES (?, ?, ?)";
+    private static final String UPDATE_QUERY =  "UPDATE INVOICE SET TOTAL_COST = ? WHERE INVOICE_ID = ?";
+    private static final String DELETE_QUERY = "DELETE FROM INVOICE WHERE INVOICE_ID = ?";
+    private static final String RETRIEVE_QUERY = "SELECT * FROM INVOICE WHERE INVOICE_ID = "
+    @Override
+    public void save(Invoice invoice) {
+
+    }
+
+    @Override
+    public void update(Invoice invoice) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public void findById(Integer id) {
+
+    }
+
+    @Override
+    public List<Invoice> findAll() {
+        return null;
+    }
+
+    @Override
+    public void close() throws Exception {
+
+    }
 }
